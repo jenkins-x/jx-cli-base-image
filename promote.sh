d970da4f5e8d94e4cd57ef3255506e954353d121
James Strachan <james.strachan@gmail.com>
2020-08-14 15:41:36 +0100

    fix: promote to jx-git-operator

--- promote.sh
+++ promote.sh
@@ -4,4 +4,6 @@ echo "promoting the new version ${VERSION} to downstream repositories"
 
 jx step create pr regex --regex "(?m)^FROM gcr.io/jenkinsxio-labs-private/jx-cli-base:(?P<version>.*)$" --version ${VERSION} --files Dockerfile --repo https://github.com/jenkins-x/jx-cli.git
 
+jx step create pr regex --regex "(?m)^FROM gcr.io/jenkinsxio-labs-private/jx-cli-base:(?P<version>.*)$" --version ${VERSION} --files Dockerfile --repo https://github.com/jenkins-x/jx-git-operator.git
+
 jx step create pr regex --regex "(?m)^FROM gcr.io/jenkinsxio-labs-private/jx-cli-base:(?P<version>.*)$" --version ${VERSION} --files Dockerfile --repo https://github.com/jenkins-x/jx-promote.git
