9930dfe4f79e5a6aa33583542a9bb2325661f31e
jstrachan <jenkins-x@googlegroups.com>
2020-09-01 15:25:09 +0100

    fix: correct promote script

--- promote.sh
+++ promote.sh
@@ -2,8 +2,8 @@
 
 echo "promoting the new version ${VERSION} to downstream repositories"
 
-jx step create pr regex --regex "(?m)^FROM gcr.io/jenkisnxio/jx-cli-base:(?P<version>.*)$" --version ${VERSION} --files Dockerfile --repo https://github.com/jenkins-x/jx-cli.git
+jx step create pr regex --regex "(?m)^FROM gcr.io/jenkinsxio/jx-cli-base:(?P<version>.*)$" --version ${VERSION} --files Dockerfile --repo https://github.com/jenkins-x/jx-cli.git
 
-jx step create pr regex --regex "(?m)^FROM gcr.io/jenkisnxio/jx-cli-base:(?P<version>.*)$" --version ${VERSION} --files Dockerfile --repo https://github.com/jenkins-x/jx-git-operator.git
+jx step create pr regex --regex "(?m)^FROM gcr.io/jenkinsxio/jx-cli-base:(?P<version>.*)$" --version ${VERSION} --files Dockerfile --repo https://github.com/jenkins-x/jx-git-operator.git
 
-jx step create pr regex --regex "(?m)^FROM gcr.io/jenkisnxio/jx-cli-base:(?P<version>.*)$" --version ${VERSION} --files Dockerfile --repo https://github.com/jenkins-x/jx-promote.git
+jx step create pr regex --regex "(?m)^FROM gcr.io/jenkinsxio/jx-cli-base:(?P<version>.*)$" --version ${VERSION} --files Dockerfile --repo https://github.com/jenkins-x/jx-promote.git
