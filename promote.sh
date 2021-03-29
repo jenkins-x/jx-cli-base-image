1f488460523640ec6c836a3d4e6932f7d93c9690
James Strachan <james.strachan@gmail.com>
2021-03-29 12:31:05 +0100

    fix: release pipeline

--- promote.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "promoting the new version ${VERSION} to downstream repositories"
-
-jx step create pr regex --regex "(?m)^FROM gcr.io/jenkinsxio/jx-cli-base-image:(?P<version>.*)$" --version ${VERSION} --files Dockerfile --repo https://github.com/jenkins-x/jx-cli.git
-
-jx step create pr regex --regex "(?m)^FROM gcr.io/jenkinsxio/jx-cli-base-image:(?P<version>.*)$" --version ${VERSION} --files Dockerfile --repo https://github.com/jenkins-x/jx-git-operator.git
-
-jx step create pr regex --regex "(?m)^FROM gcr.io/jenkinsxio/jx-cli-base-image:(?P<version>.*)$" --version ${VERSION} --files Dockerfile --repo https://github.com/jenkins-x/bdd-jx.git
